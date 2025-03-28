fa2908f470bfd79d273f11aeb02242bb9843b87e
LindaTancini <lindatancini95@gmail.com>
2025-03-28 15:44:49 +0100

    feat: associated route to the function in postController

--- routes/posts.js
+++ routes/posts.js
@@ -2,36 +2,15 @@
 const express = require("express");
 // CREO IL ROUTER
 const router = express.Router();
-//IMPORTO ARRAY CON POSTS
-const postsArray = require("../data/posts.js");
+//IMPORTO LE FUNZIONI
+const postControlls = require("../controllers/postController.js");
 //ESPORTO IL ROUTER
 module.exports = router;
 
 // CRUD
-
 // INDEX -> visualizzo tutti i posts
-router.get("/", (req, res) => {
-  res.json(postsArray);
-});
-
+router.get("/", postControlls.index);
 // SHOW -> visualizzo un solo post
-router.get("/:id", (req, res) => {
-  const currentPostId = req.params.id;
-  const currentPost = postsArray.find(
-    (element) => element.id === parseInt(currentPostId)
-  );
-  res.json(currentPost);
-});
-
+router.get("/:id", postControlls.show);
 // DESTROY -> cancello un post
-router.delete("/:id", (req, res) => {
-  const deletePostId = req.params.id;
-  const deletePost = postsArray.find(
-    (element) => element.id === parseInt(deletePostId)
-  );
-  // RIMUOVO IL POST CON SPLICE
-  postsArray.splice(postsArray.indexOf(deletePost), 1);
-  // STAMPO LA LISTA SENZA IL CONTENUTO RIMOSSO
-  console.log("Nuova lista:", postsArray);
-  res.send(deletePost);
-});
+router.delete("/:id", postControlls.destroy);

70852a44b4f3c98552ccd4547a7ca7b854d0d091
LindaTancini <lindatancini95@gmail.com>
2025-03-28 16:52:31 +0100

    feat: added bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,7 +3,13 @@ const postsArray = require("../data/posts.js");
 
 // OTTENGO TUTTI I POST, CREO FUNZIONE INDEX
 function index(req, res) {
-  res.json(postsArray);
+  let filteredPosts = postsArray;
+  if (req.query.tag) {
+    filteredPosts = postsArray.filter((element) =>
+      element.tags.includes(req.query.tag)
+    );
+  }
+  res.json(filteredPosts);
 }
 
 //OTTENGO UN POST CERCANDO IL SUO ID, CREO FUNZIONE SHOW
@@ -12,9 +18,17 @@ function show(req, res) {
   const currentPost = postsArray.find(
     (element) => element.id === parseInt(currentPostId)
   );
+  // CONTROLLO SE L'ID ESISTE
+  if (!currentPost) {
+    return res.status(404).json({
+      error: "Not Found",
+      message: "Post non trovato",
+    });
+  }
+
+  // Se il post esiste, restituisco il post
   res.json(currentPost);
 }
-
 //  ELIMINO UN POST CERCANDO IL SUO ID, CREO FUNZIONE DESTROY
 function destroy(req, res) {
   const deletePostId = req.params.id;
@@ -23,6 +37,13 @@ function destroy(req, res) {
   );
   // RIMUOVO IL POST CON SPLICE
   postsArray.splice(postsArray.indexOf(deletePost), 1);
+  // CONTRO SE L'ID ESISTE
+  if (!deletePost) {
+    return res.status(404).json({
+      error: "Not Found",
+      message: "Post non trovato",
+    });
+  }
   // STAMPO LA LISTA SENZA IL CONTENUTO RIMOSSO
   console.log("Nuova lista:", postsArray);
   res.send(deletePost);

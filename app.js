a7059a55c9ffbb8601fa70ff0ce73fa37a224a40
LindaTancini <lindatancini95@gmail.com>
2025-03-28 14:49:11 +0100

    feat: imported router

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 // DEFINISCO LA PORTA
 const port = 3000;
 
+// IMPORTO IL ROUTER DOVE SONO PRESENTI I POSTS
+const postsRouter = require("./routes/posts.js");
+// CON USE INDICO LE ROTTE POSTS
+app.use("/posts", postsRouter);
+
 // AVVIO IL SERVER SULLA PORTA 3000 E CONTROLLO SU POSTMAN
 app.listen(port, () => {
   console.log(`Sono un server attivo sulla porta:${port}`);

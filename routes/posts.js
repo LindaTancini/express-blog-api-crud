41bafb6a7b8a0be5cb5791dcc8339e6919a2c2b4
LindaTancini <lindatancini95@gmail.com>
2025-03-28 15:24:04 +0100

    feat: added route destroy

--- routes/posts.js
+++ routes/posts.js
@@ -13,6 +13,7 @@ module.exports = router;
 router.get("/", (req, res) => {
   res.json(postsArray);
 });
+
 // SHOW -> visualizzo un solo post
 router.get("/:id", (req, res) => {
   const currentPostId = req.params.id;
@@ -20,4 +21,17 @@ router.get("/:id", (req, res) => {
     (element) => element.id === parseInt(currentPostId)
   );
   res.json(currentPost);
+});
+
+// DESTROY -> cancello un post
+router.delete("/:id", (req, res) => {
+  const deletePostId = req.params.id;
+  const deletePost = postsArray.find(
+    (element) => element.id === parseInt(deletePostId)
+  );
+  // RIMUOVO IL POST CON SPLICE
+  postsArray.splice(postsArray.indexOf(deletePost), 1);
+  // STAMPO LA LISTA SENZA IL CONTENUTO RIMOSSO
+  console.log("Nuova lista:", postsArray);
+  res.send(deletePost);
 });

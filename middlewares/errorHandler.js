5c9365a490179e4d9a7c6b15cc3e4ea89f09da93
LindaTancini <lindatancini95@gmail.com>
2025-04-01 12:58:14 +0200

    feat: exported function

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,10 @@
+//CREO FUNZIONE PER ERRORE 505
 function errorHandler(err, req, res, next) {
   res.status(500).json({
     error: err.message,
     message: "Ahia!",
   });
 }
+
+//ESPORTO FUNZIONE
+module.exports = errorHandler;
